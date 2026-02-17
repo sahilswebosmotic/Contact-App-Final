85176a4285fbe6075ac3655e2a8aad34bd2e8154
sahilswebosmotic <sahils.webosmotic@gmail.com>
2026-02-17 18:33:00 +0530

    final code

---
+++ src/AppLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddContact from './Components/AddContact'
+import TableContact from './Components/TableContact'
+
+const AppLayout = () => {
+  return (
+    <>
+    <AddContact/>
+    <TableContact /> 
+    </>
+  )
+}
+
+export default AppLayout
